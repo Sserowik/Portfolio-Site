04ea552d1de40a150e7869abde4099f0948f5d58
Sserowik <sejohnserowik@gmail.com>
2023-11-05 14:14:07 -0700

    clean up changes as per request. files and assets renamed with lowercase

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
 console.log("Hiya, again. You found me. Hope you like the portfolio.")
+
+
+var text1 = "Hello";
+var text2 = "Hello, spaces are also OK!";
+var text3 = 'Single quotes are valid too.';
+var escapedText1 = "He said: \"Yes!\", that's for sure";
+var escapedText2 = 'He said: "Yes!", that\'s for sure';
+
+console.log (text1, text2)
+
+var numberInteger = 456;
+var numberInteger2 = -345; // This is negative integer
+var numberFloat = 50.25;
+
+var boolean1 = true;
+var boolean2 = false;
+var notABoolean = "false"; //This is just the string "false", not false!
+
+var emptyVariable = null;
+var notNullButAString = "null";
+console.log(doesntExist); // Outputs undefined
+console.log(emptyVariable); // Outputs Null
+console.log(notNullButAString); // Outputs "null" - not the same as null!
+
+var chocolate = null
